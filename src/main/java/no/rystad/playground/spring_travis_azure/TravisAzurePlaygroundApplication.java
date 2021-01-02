c1b0345ab66fcd312918c43227a01e6b36d61cf9
Frode Rystad <frode-git@rystad.no>
2021-01-02 10:26:36 +0100

    Scaffolding, by Spring Initializr

---
+++ src/main/java/no/rystad/playground/spring_travis_azure/TravisAzurePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package no.rystad.playground.spring_travis_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisAzurePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisAzurePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/rystad/playground/spring_travis_azure/TravisAzurePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package no.rystad.playground.spring_travis_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisAzurePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
